62cb572d790e3f6fb3af839c1dd76e9d4a420a23
Stefan <langestefan@msn.com>
2021-12-03 00:36:38 +0100

    Add ability to train k-means NN
    Signed-off-by: Stefan <langestefan@msn.com>

--- assignment_2_Kmeans_algorithm/main.py
+++ assignment_2_Kmeans_algorithm/main.py
@@ -9,10 +9,12 @@ from scipy.spatial.distance import euclidean
 from scipy import stats
 from numpy.random import default_rng
 import numpy as np
+from collections import defaultdict
 
 
-def visualize_data(data, label):
+def visualize_data(data, means, label):
     """
+    :param means:
     :param data: x/y data
     :param label: labels
     """
@@ -26,12 +28,13 @@ def visualize_data(data, label):
 
     # Plot the training points
     plt.scatter(data[:, 0], data[:, 1], c=label, cmap=plt.cm.Set1, edgecolor="black")
+    plt.scatter(means[:, 0], means[:, 1], c='blue', marker='x')
     plt.xlabel("Petal length")
     plt.ylabel("Petal width")
 
     plt.xlim(x_min, x_max)
     plt.ylim(y_min, y_max)
-    plt.title("red=setosa, orange=versicolor, grey=virginica")
+    plt.title("red=setosa, orange=versicolor, grey=virginica, blue=cluster mean")
 
     plt.show()
 
@@ -76,28 +79,60 @@ def calculate_distances(data, point):
     return distance
 
 
+def update_k_means(data, k_means, k):
+    """
+    :param data: Training data
+    :param k_means: Input K mean points
+    :param k: K umber of mean points
+    :return: Updated K mean points after one iteration
+    """
+    # distance matrix. Contains distance from k points to other points
+    size = np.size(data, 0)
+    distance_matrix = np.zeros((size, k))
+
+    # default dictionary. Creates object if an object is accessed at index that doesnt exist
+    point_dict = defaultdict(list)
+
+    # for each mean point we calculate the distance to the other points
+    for idx, point in enumerate(k_means):
+        distance_matrix[:, idx] = calculate_distances(data, point)
+
+    # for each point in our train set we check which mean is closest
+    # then we create k-clusters of data by assigning the train points to a mean point
+    for idx, point in enumerate(data):
+        k_mean = int((np.where(distance_matrix[idx] == np.amin(distance_matrix[idx])))[0])
+        point_dict[k_mean] = point_dict[k_mean] + [point]
+
+    # we sum over the column entries and divide by N to get the average of all assigned points
+    for idk in range(k):
+        k_means[idk] = np.sum(point_dict[idk], axis=0) / np.size(point_dict[idk], 0)
+
+    return k_means
+
+
 def app_main():
     """ App main """
     iris = datasets.load_iris()
     data = iris.data[:, (2, 3)]  # take first 2 features (petal length/width)
     label = iris.target
     k = 3  # number of k-means
+    itt = 10  # number of iterations
 
     # split into train test sets
     x_train, x_test, y_train, y_test = train_test_split(data, label, test_size=0.2, random_state=False)
 
-    # distance matrix. Contains distance from k points to other points
-    size = np.size(x_train, 0)
-    distance_matrix = np.zeros((size, k))
-    # visualize_data(data, label)
+    # select initial means
+    k_means = select_k_initial_means(x_train, k)
 
-    k_init_means = select_k_initial_means(x_train, k)
+    # train the model
+    for i in range(itt):
+        k_means = update_k_means(x_train, k_means, k)
 
-    for idx, point in enumerate(k_init_means):
-        distance_matrix[:, idx] = calculate_distances(x_train, point)
-        
+    # show our updated means
+    print(k_means)
 
-    print(distance_matrix)
+    # visualize the data
+    visualize_data(x_train, k_means, y_train)
 
 
 if __name__ == "__main__":
