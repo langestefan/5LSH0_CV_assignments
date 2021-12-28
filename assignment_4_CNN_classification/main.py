1f5eedfe3137760c815abbdb61b79f425dcb3455
Stefan <langestefan@msn.com>
2021-12-28 15:04:30 +0100

    Test NN training without dropout
    Signed-off-by: Stefan <langestefan@msn.com>

--- assignment_4_CNN_classification/main.py
+++ assignment_4_CNN_classification/main.py
@@ -55,22 +55,23 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
+        # self.conv2_drop = nn.Dropout2d()
         self.fc1 = nn.Linear(320, 50)
         self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        # x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
         x = x.view(-1, 320)
         x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
+        # x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x)
 
 
 if __name__ == '__main__':
-    n_epochs = 3
+    n_epochs = 1
     batch_size_train = 64
     batch_size_test = 1000
     learning_rate = 0.01
@@ -115,10 +116,27 @@ if __name__ == '__main__':
         train(epoch)
         test()
 
-    fig = plt.figure()
-    plt.plot(train_counter, train_losses, color='blue')
-    plt.scatter(test_counter, test_losses, color='red')
-    plt.legend(['Train Loss', 'Test Loss'], loc='upper right')
-    plt.xlabel('number of training examples seen')
-    plt.ylabel('negative log likelihood loss')
+    # fig1 = plt.figure()
+    # plt.plot(train_counter, train_losses, color='blue')
+    # plt.scatter(test_counter, test_losses, color='red')
+    # plt.legend(['Train Loss', 'Test Loss'], loc='upper right')
+    # plt.xlabel('number of training examples seen')
+    # plt.ylabel('negative log likelihood loss')
+    # plt.show()
+
+    examples = enumerate(test_loader)
+    batch_idx, (example_data, example_targets) = next(examples)
+
+    with torch.no_grad():
+        output = network(example_data)
+
+    fig2 = plt.figure()
+    for i in range(6):
+        plt.subplot(2, 3, i + 1)
+        plt.tight_layout()
+        plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+        plt.title("Prediction: {}".format(
+            output.data.max(1, keepdim=True)[1][i].item()))
+        plt.xticks([])
+        plt.yticks([])
     plt.show()
