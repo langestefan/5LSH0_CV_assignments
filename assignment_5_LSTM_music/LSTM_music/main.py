eda8c847426b63248393471fe4bdff93fab6cf3f
Stefan <langestefan@msn.com>
2022-04-17 01:56:19 +0200

    Play around with new configs

--- assignment_5_LSTM_music/LSTM_music/main.py
+++ assignment_5_LSTM_music/LSTM_music/main.py
@@ -3,14 +3,15 @@ from wrappers import train, eval
 if __name__ == '__main__':
     # Define your own paths here
     configs = [
-        './configs/broad_config.yaml',
-        './configs/std_config.yaml',
-        './configs/deep_config.yaml'
+        # './configs/broad_config.yaml',
+        # './configs/std_config.yaml',
+        # './configs/deep_config.yaml',
+        './configs/custom_config.yaml'
     ]
     # For each config file we will train and generate songs
     for c in configs:
-        # print('Training: {}'.format(c))
-        # train.train(c)
+        print('Training: {}'.format(c))
+        train.train(c)
         print('Testing: {}'.format(c))
         eval.test(c)
 
